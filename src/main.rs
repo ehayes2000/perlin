745232e79d68fb304e91b6fb69b37367f13b9e0b
ehayes <eqhayes@protonmail.com>
2025-09-27 13:48:10 -0400

    better grid

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use math::*;
 
 const WINDOW_WIDTH: usize = 640;
 const WINDOW_HEIGHT: usize = 480;
-const GRID_SIZE: usize = 10;
+const GRID_SIZE: usize = 40;
 const GRID_W: usize = WINDOW_WIDTH / GRID_SIZE + 1;
 const GRID_H: usize = WINDOW_HEIGHT / GRID_SIZE + 1;
 
@@ -29,6 +29,64 @@ fn unit(angle: f64) -> Vector2 {
     Vector2::new(angle.cos() as f32, angle.sin() as f32)
 }
 
+struct Cell<'a> {
+    pub corners: &'a [Vector2; 4],
+    pub abs_offset_x: usize,
+    pub abs_offset_y: usize,
+}
+
+fn process_cell(cell: Cell, draw_handle: &mut RaylibDrawHandle) {
+    let find_dots = |x: f32, y: f32| {
+        [(x, y), (x - 1., y), (x, y - 1.), (x - 1., y - 1.)]
+            .into_iter()
+            .map(|(x, y)| Vector2::new(x, y))
+            .zip(cell.corners)
+            .map(|(offset, corner)| offset.dot(*corner))
+            .collect::<Vec<_>>()
+    };
+
+    let samples = 3;
+    let size: f32 = 1. / samples as f32;
+    let sample_offsets = {
+        let mut offsets: Vec<(f32, f32)> = Vec::with_capacity(samples * samples);
+        for x in 0..samples {
+            for y in 0..samples {
+                offsets.push((x as f32 * size, y as f32 * size))
+            }
+        }
+        offsets
+    };
+
+    for (x, y) in sample_offsets {
+        let dots = find_dots(x, y);
+        let rel_radius = bilerp(
+            dots[0],
+            dots[1],
+            dots[2],
+            dots[3],
+            smootherstep(x),
+            smootherstep(y),
+        ) * size
+            / 2.;
+
+        if rel_radius > 1. {
+            panic!("bad radius");
+        }
+
+        let abs_radius = rel_radius.abs() * GRID_SIZE as f32;
+        let abs_x = cell.abs_offset_x as f32 + (x + size / 2.) * GRID_SIZE as f32;
+        let abs_y = cell.abs_offset_y as f32 + (y + size / 2.) * GRID_SIZE as f32;
+        let red = (((100. + (rel_radius.abs() / (size / 2.))).clamp(0., 1.)) * 255.) as u8;
+
+        draw_handle.draw_circle(
+            abs_x as i32,
+            abs_y as i32,
+            abs_radius,
+            Color::new(255, 0, 0, red),
+        );
+    }
+}
+
 fn main() {
     let (mut rl, thread) = raylib::init()
         .size(WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32)
@@ -37,47 +95,30 @@ fn main() {
     let mut grid: Grid = [[0.0; GRID_W]; GRID_H];
     let mut angle_offset: f64 = 0.0;
 
-    let offset_vectors = [(-0.5, -0.5), (0.5, -0.5), (-0.5, 0.5), (0.5, 0.5)]
-        .into_iter()
-        .map(|(a, b)| Vector2::new(a, b))
-        .collect::<Vec<_>>();
     set_grid(&mut grid, &|_| random_angle());
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::WHITE);
-        angle_offset += 0.015;
+        angle_offset += 0.01;
+
         for i in 0..GRID_H - 1 {
             for j in 0..GRID_W - 1 {
-                let y = i * GRID_SIZE + GRID_SIZE / 2;
-                let x = j * GRID_SIZE + GRID_SIZE / 2;
-
                 let corners = [
-                    grid[i][j] + angle_offset,
-                    grid[i][j + 1] + angle_offset,
-                    grid[i + 1][j] + angle_offset,
-                    grid[i + 1][j + 1] + angle_offset,
-                ]
-                .into_iter()
-                .map(unit)
-                .collect::<Vec<_>>();
+                    unit(grid[i][j] + angle_offset),
+                    unit(grid[i][j + 1] + angle_offset),
+                    unit(grid[i + 1][j] + angle_offset),
+                    unit(grid[i + 1][j + 1] + angle_offset),
+                ];
 
-                let dots = offset_vectors
-                    .iter()
-                    .zip(corners.iter())
-                    .map(|(offset, corner)| offset.dot(*corner))
-                    .collect::<Vec<_>>();
-
-                let interped = bilerp(
-                    dots[0] as f64,
-                    dots[1] as f64,
-                    dots[2] as f64,
-                    dots[3] as f64,
-                    smootherstep(0.5),
-                    smootherstep(0.5),
-                ) * GRID_SIZE as f64;
-
-                d.draw_circle(x as i32, y as i32, interped as f32, Color::RED);
+                process_cell(
+                    Cell {
+                        corners: &corners,
+                        abs_offset_x: j * GRID_SIZE,
+                        abs_offset_y: i * GRID_SIZE,
+                    },
+                    &mut d,
+                );
             }
         }
     }
--- src/math.rs
+++ src/math.rs
@@ -1,19 +1,19 @@
 #![allow(unused)]
 
-pub fn lerp(v0: f64, v1: f64, t: f64) -> f64 {
+pub fn lerp(v0: f32, v1: f32, t: f32) -> f32 {
     (1.0 - t) * v0 + t * v1
 }
 
-pub fn bilerp(tl: f64, tr: f64, bl: f64, br: f64, u: f64, v: f64) -> f64 {
+pub fn bilerp(tl: f32, tr: f32, bl: f32, br: f32, u: f32, v: f32) -> f32 {
     let top = lerp(tl, tr, u);
     let bot = lerp(bl, br, u);
     lerp(top, bot, v)
 }
 
-pub fn smoothstep(x: f64) -> f64 {
+pub fn smoothstep(x: f32) -> f32 {
     x * x * (3.0 - 2.0 * x)
 }
 
-pub fn smootherstep(x: f64) -> f64 {
+pub fn smootherstep(x: f32) -> f32 {
     x * x * x * (x * (6.0 * x - 15.0) + 10.0)
 }
